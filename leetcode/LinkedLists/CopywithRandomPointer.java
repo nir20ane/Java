1f8392aa142a2e5a0378d951b7834c6a68a6858b
nir20ane <nirranjhane@gmail.com>
2019-01-30 13:21:52 -0800

    Update CopywithRandomPointer.java

--- leetcode/LinkedLists/CopywithRandomPointer.java
+++ leetcode/LinkedLists/CopywithRandomPointer.java
@@ -1,5 +1,27 @@
-package MyLinked;
-
-public class CopywithRandomPointer {
-
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode curr = head;
+        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        while(curr != null)
+        {
+            map.put(curr, new RandomListNode(curr.label));
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr != null)
+        {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+       return map.get(head) ;
+    }
 }
